e948351ee1e2bcac7faa5a243a95e560bc3640a9
ALEXPERRO <alessio.perro10@gmail.com>
2026-07-09 19:45:50 +0200

    feat(home): rebuild About Me hero as an asymmetric editorial layout

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { ContentService } from '../../core/services/content.service';
+import { Icon } from '../../shared/icon/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, Icon],
   templateUrl: './home.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
